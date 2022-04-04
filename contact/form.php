5faf084bd4d8281ad052ac2ecce806ceecb15f41
Orrin Manning <orrin.manning@protonmail.com>
2022-04-04 12:15:48 -0500

    Simplify contact form

--- contact/form.php
+++ contact/form.php
@@ -1,27 +1,13 @@
 <form class="contact-form" id="contact-form">
-  <ul class="contact-form-fields">
-    <li class="contact-form-item">
-      <label class="serif" for="name">Name:</label>
-      <input class="contact-form-input" id="name" type="text" name="name" form="contact-form">
-    </li>
-    <li class="contact-form-item">
-      <label class="serif" for="email">Email Address:</label>
-      <input class="contact-form-input" id="email" type="email" name="email" form="contact-form">
-    </li>
-    <li class="contact-form-item">
-      <label class="serif" for="organization">Organization:</label>
-      <input class="contact-form-input" id="organization" type="text" name="organization" form="contact-form">
-    </li>
-    <li class="contact-form-item">
-      <label class="serif" for="role">Role:</label>
-      <input class="contact-form-input" id="role" type="text" name="role" form="contact-form">
-    </li>
-    <li class="contact-form-item">
-      <label class="serif" for="description">
-        Please provide a brief description of the development needs for your organization:
-      </label>
-      <textarea class="contact-form-input big-input" id="description" type="text" name="description" rows="5" form="contact-form"></textarea>
-    </li>
-  </ul>
+  <label for="name">Name:</label>
+  <input class="contact-form-input" id="name" type="text" name="name" form="contact-form">
+  <label for="email">Email Address:</label>
+  <input class="contact-form-input" id="email" type="email" name="email" form="contact-form">
+  <label for="organization">Organization:</label>
+  <input class="contact-form-input" id="organization" type="text" name="organization" form="contact-form">
+  <label for="role">Role:</label>
+  <input class="contact-form-input" id="role" type="text" name="role" form="contact-form">
+  <label for="description">Description:</label>
+  <textarea class="contact-form-input big-input" id="description" type="text" name="description" rows="5" form="contact-form"></textarea>
   <button class="contact-submit-btn" type="submit">Submit</button>
 </form>
