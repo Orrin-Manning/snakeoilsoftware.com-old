f3197c86d4a22994c1f01896c62cd04dceda87e7
Orrin Manning <orrin.manning@protonmail.com>
2022-04-04 14:01:45 -0500

    Create script for creating mySQL connections

---
+++ contact/includes/dbh.inc.php
@@ -0,0 +1,11 @@
+<?php
+include 'dbAccess.inc.php';
+
+// Create connection
+$conn = new mysqli($serverName, $username, $password, $dbName);
+
+// Check Connection
+if ($conn->connect_error)
+{
+  die('Connection failed: ' . $conn->connect_error);
+}
