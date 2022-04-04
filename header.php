af90a0ed0568e12e54154759702277ee7706e055
Orrin Manning <orrin.manning@protonmail.com>
2022-04-04 03:15:46 -0500

    Expand to include entire top of document before main tag

--- header.php
+++ header.php
@@ -1,5 +1,17 @@
-<header class="site-header">
-  <a href="/">
-    <h1>Snake Oil Software</h1>
-  </a>
-</header>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title><?php echo $title ?></title>
+  <meta name="author" content="<?php echo $author ?>">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../styles.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Rye">
+</head>
+<body>
+  <header class="site-header">
+    <a href="/">
+      <h1>Snake Oil Software</h1>
+    </a>
+  </header>
