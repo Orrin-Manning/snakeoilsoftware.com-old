a106cee53df2a0634e66ead8e05291a79c337f12
Orrin Manning <orrin.manning@protonmail.com>
2022-04-04 03:41:34 -0500

    Move contact form into dedicated file

---
+++ contact/form.php
@@ -0,0 +1,31 @@
+<form class="contact-form" id="contact-form">
+  <ul class="contact-form-fields">
+    <li class="contact-form-item">
+      <label class="serif" for="first-name">First Name:</label>
+      <input class="contact-form-input" id="first-name" type="text" name="first-name" form="contact-form">
+    </li>
+    <li class="contact-form-item">
+      <label class="serif" for="last-name">Last Name:</label>
+      <input class="contact-form-input" id="last-name" type="text" name="last-name" form="contact-form">
+    </li>
+    <li class="contact-form-item">
+      <label class="serif" for="email">Email Address:</label>
+      <input class="contact-form-input" id="email" type="email" name="email" form="contact-form">
+    </li>
+    <li class="contact-form-item">
+      <label class="serif" for="organization">Organization:</label>
+      <input class="contact-form-input" id="organization" type="text" name="organization" form="contact-form">
+    </li>
+    <li class="contact-form-item">
+      <label class="serif" for="role">Role:</label>
+      <input class="contact-form-input" id="role" type="text" name="role" form="contact-form">
+    </li>
+    <li class="contact-form-item">
+      <label class="serif" for="description">
+        Please provide a brief description of the development needs for your organization:
+      </label>
+      <textarea class="contact-form-input big-input" id="description" type="text" name="description" form="contact-form"></textarea>
+    </li>
+  </ul>
+  <button class="contact-submit-btn" type="submit">Submit</button>
+</form>
