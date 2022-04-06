21432401bdf2827015bbdcebdba3499574635bca
Orrin Manning <orrin.manning@protonmail.com>
2022-04-06 00:21:01 -0500

    Guard against bypassing the submit button

--- contact/includes/form.inc.php
+++ contact/includes/form.inc.php
@@ -1,3 +1,9 @@
 <?php
 // Get connection to the database
 include 'dbh.inc.php';
+
+// Verify that this page was reached appropriately
+if (!isset($_POST['submit']))
+{
+  header('location: ../');
+}
