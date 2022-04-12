3a4f53241c80f55251e7eb29dda506939f2a519b
Orrin Manning <orrin.manning@protonmail.com>
2022-04-12 04:57:16 -0500

    Remove h2 tags from site-nav links

--- header.php
+++ header.php
@@ -22,13 +22,9 @@
   </header>
   <ul class="site-nav">
     <li>
-      <a href="/about">
-        <h2 class="site-nav-button-text">About</h2>
-      </a>
+      <a href="/about">About</a>
     </li>
     <li>
-      <a href="/contact">
-        <h2 class="site-nav-button-text">Contact</h2>
-      </a>
+      <a href="/contact">Contact</a>
     </li>
   </ul>
