1f1248727e447bdb7c592e89546c44ff3a4bb895
Orrin Manning <orrin.manning@protonmail.com>
2022-04-12 04:46:17 -0500

    Remove site-nav

--- index.php
+++ index.php
@@ -5,18 +5,6 @@ include_once 'header.php';
 ?>
 <main>
   <h1 class="section-header">Affordable Software Development</h1>
-  <ul class="site-nav">
-    <li>
-      <a href="about">
-        <h2 class="site-nav-button-text">About</h2>
-      </a>
-    </li>
-    <li>
-      <a href="contact">
-        <h2 class="site-nav-button-text">Contact</h2>
-      </a>
-    </li>
-  </ul>
 </main>
 <?php
 require_once 'footer.php';
