b41e8117057d65547ee4dafe14f67ad986a7073c
Orrin Manning <orrin.manning@protonmail.com>
2022-04-12 04:52:28 -0500

    Change site-nav references to absolute paths

--- header.php
+++ header.php
@@ -22,12 +22,12 @@
   </header>
   <ul class="site-nav">
     <li>
-      <a href="about">
+      <a href="/about">
         <h2 class="site-nav-button-text">About</h2>
       </a>
     </li>
     <li>
-      <a href="contact">
+      <a href="/contact">
         <h2 class="site-nav-button-text">Contact</h2>
       </a>
     </li>
