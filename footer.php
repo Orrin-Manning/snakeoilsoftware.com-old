f88ca812871c5bc91317fac4e9859d6b3820b81d
Orrin Manning <orrin.manning@protonmail.com>
2022-04-22 15:52:58 -0500

    Include bootstrap js

--- footer.php
+++ footer.php
@@ -1,3 +1,9 @@
   <p class="copyright">Copyright &copy; 2022 Snake Oil Software</p>
+  <script
+    src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+    crossorigin="anonymous"
+    >
+  </script>
 </body>
 </html>
