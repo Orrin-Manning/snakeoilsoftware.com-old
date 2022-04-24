4769d243a31b811064349e886ba93134ba4929d0
Orrin Manning <orrin.manning@protonmail.com>
2022-04-24 12:58:31 -0500

    Adopt bs form-control styling

--- contact/form.php
+++ contact/form.php
@@ -1,13 +1,23 @@
 <form action="includes/form.inc.php" method="post" class="contact-form" id="contact-form">
-  <label for="name">Name:</label>
-  <input class="contact-form-input" id="name" type="text" name="name" form="contact-form">
-  <label for="email">Email Address:</label>
-  <input class="contact-form-input" id="email" type="email" name="email" form="contact-form">
-  <label for="organization">Organization:</label>
-  <input class="contact-form-input" id="organization" type="text" name="organization" form="contact-form">
-  <label for="role">Role:</label>
-  <input class="contact-form-input" id="role" type="text" name="role" form="contact-form">
-  <label for="description">Description:</label>
-  <textarea class="contact-form-input big-input" id="description" type="text" name="description" rows="5" form="contact-form"></textarea>
+  <div class="mb-3">
+    <label class="form-label" for="name">Name:</label>
+    <input class="form-control" id="name" type="text" name="name" form="contact-form">
+  </div>
+  <div class="mb-3">
+    <label class="form-label" for="email">Email Address:</label>
+    <input class="form-control" id="email" type="email" name="email" form="contact-form">
+  </div>
+  <div class="mb-3">
+    <label class="form-label" for="organization">Organization:</label>
+    <input class="form-control" id="organization" type="text" name="organization" form="contact-form">
+  </div>
+  <div class="mb-3">
+    <label class="form-label" for="role">Role:</label>
+    <input class="form-control" id="role" type="text" name="role" form="contact-form">
+  </div>
+  <div class="mb-3">
+    <label class="form-label" for="description">Description:</label>
+    <textarea class="form-control" id="description" type="text" name="description" rows="5" form="contact-form"></textarea>
+  </div>
   <button class="contact-submit-btn" type="submit" name="submit" value="submit" form="contact-form">Submit</button>
 </form>
