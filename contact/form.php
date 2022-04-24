c57411568509370155cab50a2eb2f4aab7ccbb18
Orrin Manning <orrin.manning@protonmail.com>
2022-04-24 13:40:12 -0500

    implement floating labels in form

--- contact/form.php
+++ contact/form.php
@@ -1,23 +1,23 @@
 <form action="includes/form.inc.php" method="post" class="contact-form" id="contact-form">
-  <div class="mb-3">
-    <label class="form-label" for="name">Name:</label>
-    <input class="form-control" id="name" type="text" name="name" form="contact-form">
+  <div class="form-floating mb-3">
+    <input class="form-control" id="name" type="text" name="name" placeholder="John Doe" form="contact-form">
+    <label class="form-label" for="name">Name</label>
   </div>
-  <div class="mb-3">
-    <label class="form-label" for="email">Email Address:</label>
-    <input class="form-control" id="email" type="email" name="email" form="contact-form">
+  <div class="form-floating mb-3">
+    <input class="form-control" id="email" type="email" name="email" placeholder="name@example.com" form="contact-form">
+    <label class="form-label" for="email">Email</label>
   </div>
-  <div class="mb-3">
-    <label class="form-label" for="organization">Organization:</label>
-    <input class="form-control" id="organization" type="text" name="organization" form="contact-form">
+  <div class="form-floating mb-3">
+    <input class="form-control" id="organization" type="text" name="organization" placeholder="Company" form="contact-form">
+    <label class="form-label" for="organization">Organization</label>
   </div>
-  <div class="mb-3">
-    <label class="form-label" for="role">Role:</label>
-    <input class="form-control" id="role" type="text" name="role" form="contact-form">
+  <div class="form-floating mb-3">
+    <input class="form-control" id="role" type="text" name="role" placeholder="Technical Director" form="contact-form">
+    <label class="form-label" for="role">Role</label>
   </div>
-  <div class="mb-3">
-    <label class="form-label" for="description">Description:</label>
-    <textarea class="form-control" id="description" type="text" name="description" rows="5" form="contact-form"></textarea>
+  <div class="form-floating mb-3">
+    <textarea class="form-control" id="description" type="text" name="description" placeholder="Describe your needs here" style="height: 150px;" form="contact-form"></textarea>
+    <label class="form-label" for="description">Description</label>
   </div>
   <button class="contact-submit-btn" type="submit" name="submit" value="submit" form="contact-form">Submit</button>
 </form>
