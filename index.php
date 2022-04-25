6dbf2ab947c1bfdf347624574de179774c92ab8f
Orrin Manning <orrin.manning@protonmail.com>
2022-04-25 11:28:48 -0500

    Tweak heading

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   require_once $_SERVER['DOCUMENT_ROOT'].'/header.php';
 ?>
 <main>
-  <h1 class="text-center">Freelance Website and Mobile App Development</h1>
+  <h1 class="display-5 text-center">Independent Website and Mobile App Development</h1>
   <div class="d-grid col-6 mx-auto">
     <a class="btn btn-light" href="/contact">Get in Touch!</a>
   </div>
