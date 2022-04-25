a549768ea2bc18c22e8131afe7f447634bfd37c0
Orrin Manning <orrin.manning@protonmail.com>
2022-04-24 22:46:49 -0500

    Change text of main heading

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   require_once $_SERVER['DOCUMENT_ROOT'].'/header.php';
 ?>
 <main>
-  <h1>Affordable Software Development</h1>
+  <h1 class="text-center">Freelance Website and Mobile App Development</h1>
 </main>
 <?php
   require_once $_SERVER['DOCUMENT_ROOT'].'/footer.php';
