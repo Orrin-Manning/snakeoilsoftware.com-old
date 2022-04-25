c2ffa2d3fdf0bed4a746c7021506123c0fd0d183
Orrin Manning <orrin.manning@protonmail.com>
2022-04-25 02:29:51 -0500

    Introduce 'call-to-action' button to homepage

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
 ?>
 <main>
   <h1 class="text-center">Freelance Website and Mobile App Development</h1>
+  <div class="d-grid col-6 mx-auto">
+    <a class="btn btn-light" href="/contact">Get in Touch!</a>
+  </div>
 </main>
 <?php
   require_once $_SERVER['DOCUMENT_ROOT'].'/footer.php';
