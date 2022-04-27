8eefcb0005d414716280d632219f75081cab3291
Orrin Manning <orrin.manning@protonmail.com>
2022-04-26 21:03:35 -0500

    Parse database credentials from external .ini file

--- contact/includes/dbh.inc.php
+++ contact/includes/dbh.inc.php
@@ -1,6 +1,14 @@
 <?php
-  include 'dbAccess.inc.php';
+  // include 'dbAccess.inc.php';
+  $credentials = parse_ini_file('.database.ini', true)['db_credentials'];
+
+  $serverName = $credentials['server_name'];
+  $username = $credentials['username'];
+  $password = $credentials['password'];
+  $dbName = $credentials['db_name'];
   
+  unset($ini);
+
   // Create connection
   $conn = new mysqli($serverName, $username, $password, $dbName);
   
