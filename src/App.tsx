ceedf3ae9c82669dad06ab1602466764014978e5
Orrin Manning <orrin.manning@protonmail.com>
2022-05-04 10:24:04 -0500

    Implement Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Header from "./components/header";
 
 export default function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 }
