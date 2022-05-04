44c7ec1bc75bf402b81fe468e4e500ca8026909e
Orrin Manning <orrin.manning@protonmail.com>
2022-05-04 13:40:32 -0500

    Implement useEffect to edit body classList

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
+import { useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function SnakeOilSoftware({ Component, pageProps }) {
-  document.body.classList.add("bg-dark", "text-light");
+  useEffect(() => document.body.classList.add("bg-dark", "text-light"));
 
   return <Component {...pageProps} />;
 }
