6f610773063e1ae338a0ebc844a242ce4c5b7318
Orrin Manning <orrin.manning@protonmail.com>
2022-05-04 13:11:55 -0500

    Apply dark theme globally

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function SnakeOilSoftware({ Component, pageProps }) {
+  document.body.classList.add("bg-dark", "text-light");
+
   return <Component {...pageProps} />;
 }
