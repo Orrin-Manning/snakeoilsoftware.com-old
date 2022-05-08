e10903adf48c42e5a7f94a8d2b3d7d9422b5f141
Orrin Manning <orrin.manning@protonmail.com>
2022-05-08 13:56:26 -0500

    Remove redundant 'main' tag

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -3,22 +3,20 @@ import Container from "react-bootstrap/Container";
 export default function About() {
   return (
     <Container as="main">
-      <main>
-        <Container as="section">
-          <h1 className="text-center">About Us</h1>
-          <p>
-            Snake Oil Software was founded by former Texas Tech students and
-            Lubbock locals. Our aim is to provide hands on software development
-            services to small businesses in the West Texas region. We believe
-            that proximity and open communication are crucial to satisying the
-            complex techincal needs of our clients. We stand behind our services
-            100% and pledge to deliver quality products.
-          </p>
-        </Container>
-        <section>
-          <h1 className="text-center">Our Team</h1>
-        </section>
-      </main>
+      <Container as="section">
+        <h1 className="text-center">About Us</h1>
+        <p>
+          Snake Oil Software was founded by former Texas Tech students and
+          Lubbock locals. Our aim is to provide hands on software development
+          services to small businesses in the West Texas region. We believe that
+          proximity and open communication are crucial to satisying the complex
+          techincal needs of our clients. We stand behind our services 100% and
+          pledge to deliver quality products.
+        </p>
+      </Container>
+      <section>
+        <h1 className="text-center">Our Team</h1>
+      </section>
     </Container>
   );
 }
