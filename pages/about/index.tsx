9c9d1ede0f8cda4d4e4588f0654e66cfa3096f89
Orrin Manning <orrin.manning@protonmail.com>
2022-05-08 13:22:56 -0500

    Implement about page in react

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,5 +1,20 @@
 import Container from "react-bootstrap/Container";
 
 export default function About() {
-  return <Container as="main"></Container>;
+  return (
+    <Container as="main">
+      <main>
+        <section>
+          <h1 className="text-center">Who We Are</h1>
+          <p className="text-center">
+            Snake Oil Software was founded by a mix of Texas Tech students and
+            Lubbock locals.
+          </p>
+        </section>
+        <section>
+          <h1 className="text-center">Our Mission</h1>
+        </section>
+      </main>
+    </Container>
+  );
 }
