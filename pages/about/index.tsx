8c7471eaf04f5f0ed4f9d9333ff9e678711e7dcd
Orrin Manning <orrin.manning@protonmail.com>
2022-05-09 12:53:56 -0500

    Implement Card for displaying team member profile

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,4 +1,8 @@
 import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+import Image from "next/image";
 
 export default function About() {
   return (
@@ -16,6 +20,32 @@ export default function About() {
       </Container>
       <Container as="section">
         <h1 className="text-center">Our Team</h1>
+        <Card className="text-dark p-3">
+          <Row>
+            <Col xs={3}>
+              <Image
+                src="/IMG_0465.jpg"
+                layout="responsive"
+                width={128}
+                height={128}
+                className="rounded-circle"
+              />
+            </Col>
+            <Col
+              xs={9}
+              className="d-flex flex-column justify-content-center ps-0"
+            >
+              <Card.Title>Orrin Manning</Card.Title>
+              <Card.Subtitle>Founder, Full Stack Developer</Card.Subtitle>
+            </Col>
+          </Row>
+          <hr />
+          <Card.Text>
+            Studied computer science at the University of Texas at Arlington and
+            professional physics at Texas Tech University, before dropping out
+            to found Snake Oil Software.
+          </Card.Text>
+        </Card>
       </Container>
     </main>
   );
