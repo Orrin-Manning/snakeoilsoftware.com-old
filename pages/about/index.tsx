edd4f95e6d69a38c2c02daea45ebae1e19fa78d6
Orrin Manning <orrin.manning@protonmail.com>
2022-05-10 11:47:51 -0500

    Wrap team member cards based on width of viewport

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -20,33 +20,37 @@ export default function About() {
       </Container>
       <Container as="section">
         <h1 className="text-center">Our Team</h1>
-        <Card className="text-dark p-3">
-          <Row>
-            <Col xs={3}>
-              <Image
-                src="/IMG_0465.jpg"
-                alt="Headshot of Orrin Manning"
-                layout="responsive"
-                width={128}
-                height={128}
-                className="rounded-circle"
-              />
-            </Col>
-            <Col
-              xs={9}
-              className="d-flex flex-column justify-content-center ps-0"
-            >
-              <Card.Title>Orrin Manning</Card.Title>
-              <Card.Subtitle>Founder, Full Stack Developer</Card.Subtitle>
-            </Col>
-          </Row>
-          <hr />
-          <Card.Text>
-            Studied computer science at the University of Texas at Arlington and
-            professional physics at Texas Tech University, before dropping out
-            to found Snake Oil Software.
-          </Card.Text>
-        </Card>
+        <Row>
+          <Col md={6} lg={4}>
+            <Card className="text-dark p-3">
+              <Row>
+                <Col xs={3}>
+                  <Image
+                    src="/IMG_0465.jpg"
+                    alt="Headshot of Orrin Manning"
+                    layout="responsive"
+                    width={128}
+                    height={128}
+                    className="rounded-circle"
+                  />
+                </Col>
+                <Col
+                  xs={9}
+                  className="d-flex flex-column justify-content-center ps-0"
+                >
+                  <Card.Title>Orrin Manning</Card.Title>
+                  <Card.Subtitle>Founder, Full Stack Developer</Card.Subtitle>
+                </Col>
+              </Row>
+              <hr />
+              <Card.Text>
+                Studied computer science at the University of Texas at Arlington
+                and professional physics at Texas Tech University, before
+                dropping out to found Snake Oil Software.
+              </Card.Text>
+            </Card>
+          </Col>
+        </Row>
       </Container>
     </Container>
   );
