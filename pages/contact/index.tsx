86b70e1163828423123e8c84931cde65ad270ce1
Orrin Manning <orrin.manning@protonmail.com>
2022-05-10 16:27:07 -0500

    Allow form to flow right on larger screens

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,4 +1,6 @@
 import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 import ContactForm from "../../components/contactForm";
 
@@ -6,15 +8,19 @@ export default function Contact() {
   return (
     <Container as="main">
       <h1 className="text-center">Contact Us</h1>
-      <section className="mb-3">
-        <p>
-          Due to the varied technological needs of our clients, service will be
-          evaluated on a case-by-case basis. Please complete this questionnaire,
-          and we will reach out to you to create a personalized plan of action
-          for your business needs.
-        </p>
-        <ContactForm />
-      </section>
+      <Row as="section" className="justify-content-around mb-3">
+        <Col md={12} lg={4}>
+          <p>
+            Due to the varied technological needs of our clients, service will
+            be evaluated on a case-by-case basis. Please complete this
+            questionnaire, and we will reach out to you to create a personalized
+            plan of action for your business needs.
+          </p>
+        </Col>
+        <Col md={8} lg={6}>
+          <ContactForm />
+        </Col>
+      </Row>
 
       <hr />
 
