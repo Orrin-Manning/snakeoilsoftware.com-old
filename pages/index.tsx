40806b838a2618f8c704f86293b0ef9542c25c10
Orrin Manning <orrin.manning@protonmail.com>
2022-05-10 10:07:50 -0500

    Allow content boxes to flow left-to-right on large screens

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
 import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 export default function Home() {
   return (
@@ -7,30 +9,32 @@ export default function Home() {
       <h1 className="display-6 text-center">
         Independent Website and Mobile App Development
       </h1>
-      <section className="mx-2">
-        <p>
-          Snake Oil Software is proudly owned and operated in
-          <span className="fw-bolder fst-italic">Lubbock, Texas</span>
-          with the aim of providing hands-on development services to individuals
-          and small businesses in the west Texas region. The last few years have
-          shown us all how vital a digital presence is, and we want to help
-          bring you or your business up to date with the current industry
-          standards.
-        </p>
-      </section>
-      <div className="card text-dark px-3 py-4">
-        <h3 className="text-center">Let&apos;s get in touch!</h3>
-        <p>
-          We would love to hear more about you and the technological needs of
-          your business! Complete the form on our contact page so we can reach
-          out to you and establish a personalized plan of action.
-        </p>
-        <div className="d-grid col-6 mx-auto">
-          <Link href="/contact">
-            <a className="btn btn-dark">Contact Us Today</a>
-          </Link>
-        </div>
-      </div>
+      <Row>
+        <Col lg className="mx-2">
+          <p>
+            Snake Oil Software is proudly owned and operated in
+            <span className="fw-bolder fst-italic">Lubbock, Texas</span>
+            with the aim of providing hands-on development services to
+            individuals and small businesses in the west Texas region. The last
+            few years have shown us all how vital a digital presence is, and we
+            want to help bring you or your business up to date with the current
+            industry standards.
+          </p>
+        </Col>
+        <Col lg className="card text-dark px-3 py-4">
+          <h3 className="text-center">Let&apos;s get in touch!</h3>
+          <p>
+            We would love to hear more about you and the technological needs of
+            your business! Complete the form on our contact page so we can reach
+            out to you and establish a personalized plan of action.
+          </p>
+          <div className="d-grid col-6 mx-auto">
+            <Link href="/contact">
+              <a className="btn btn-dark">Contact Us Today</a>
+            </Link>
+          </div>
+        </Col>
+      </Row>
     </Container>
   );
 }
