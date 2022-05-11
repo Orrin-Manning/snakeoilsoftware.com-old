bd23ca431f778391976ae98b451f2283929db667
Orrin Manning <orrin.manning@protonmail.com>
2022-05-11 04:02:22 -0500

    Track pageview on change in route

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,25 @@
+import { useRouter } from "next/router";
 import { useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/globals.scss";
 import SiteNav from "../components/sitenav";
 import Footer from "../components/footer";
+import { pageview } from "../lib/gtag";
 
 export default function SnakeOilSoftware({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    router.events.on("hashChangeComplete", handleRouteChange);
+    return () => {
+      router.events.on("routeChangeComplete", handleRouteChange);
+      router.events.on("hashChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
       <SiteNav />
