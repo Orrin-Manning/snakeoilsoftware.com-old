5b48c4177cb6d066b31d99fd5b84b3f6aca47c74
Orrin Manning <orrin.manning@protonmail.com>
2022-05-11 04:02:03 -0500

    Initialize google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,29 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
+import { GA_TRACKING_ID } from "../lib/gtag";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-NXQXPK9159"
+        />
+        <Script
+          id="gtag-init"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', '${GA_TRACKING_ID}');
+            `,
+          }}
+        />
+      </Head>
       <body className="bg-dark text-light">
         <Main />
         <NextScript />
