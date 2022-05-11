7969028e233bedb1c96bbbb6073c02bba7f4fff9
Orrin Manning <orrin.manning@protonmail.com>
2022-05-11 05:36:35 -0500

    Move analytics scripts to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,17 @@
 import { useRouter } from "next/router";
+import Script from "next/script";
 import { useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/globals.scss";
 import SiteNav from "../components/sitenav";
 import Footer from "../components/footer";
-import { pageview } from "../lib/gtag";
+import * as gtag from "../lib/gtag";
 
 export default function SnakeOilSoftware({ Component, pageProps }) {
   const router = useRouter();
   useEffect(() => {
     const handleRouteChange = (url: URL) => {
-      pageview(url);
+      gtag.pageview(url);
     };
     router.events.on("routeChangeComplete", handleRouteChange);
     router.events.on("hashChangeComplete", handleRouteChange);
@@ -22,6 +23,26 @@ export default function SnakeOilSoftware({ Component, pageProps }) {
 
   return (
     <>
+      {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-NXQXPK9159"
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       <SiteNav />
       <Component {...pageProps} />
       <Footer className="mt-3" />
