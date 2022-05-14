14cef23fdcc33e7d0c12a11df42e783d20f7d12c
Orrin Manning <orrin.manning@protonmail.com>
2022-05-14 10:35:15 -0500

    create file for handling connection to database

---
+++ lib/dbConnect.ts
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const uri = process.env.MONGODB_URI;
+
+if (!uri) {
+  throw new Error("Please add your Mongo URI to .env.local");
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function dbConnect() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+
+    cached.promise = mongoose.connect(uri, opts).then((mongoose) => {
+      return mongoose;
+    });
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+
+export default dbConnect;
