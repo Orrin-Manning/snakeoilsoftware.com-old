97d0c84f43904a871b2ccbdc747320c98ac77917
Orrin Manning <orrin.manning@protonmail.com>
2022-05-15 14:39:34 -0500

    Provide type for form error

--- components/contactForm.tsx
+++ components/contactForm.tsx
@@ -4,6 +4,14 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import FloatingLabel from "react-bootstrap/FloatingLabel";
 
+type contactFormError = {
+  name?: String;
+  email?: String;
+  organization?: String;
+  role?: String;
+  description?: String;
+};
+
 export default function ContactForm() {
   const [form, setForm] = useState({
     name: "",
