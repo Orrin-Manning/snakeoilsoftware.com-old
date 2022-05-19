7ae0e7496ffeb438ddb72f02bbc2e2023219ee50
Orrin Manning <orrin.manning@protonmail.com>
2022-05-19 13:07:55 -0500

    Fix large icon bug with FontAwesome components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
 import { useRouter } from "next/router";
 import Script from "next/script";
 import { useEffect } from "react";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/globals.scss";
 import SiteNav from "../components/sitenav";
