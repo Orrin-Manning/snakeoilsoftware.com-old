769b2d57c804f84871d59c8fea46670eba82250a
Orrin Manning <orrin.manning@protonmail.com>
2022-05-20 03:16:31 -0500

    Improve flow of the page

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,22 +12,21 @@ export default function Home() {
       <Head>
         <title>Snake Oil Software</title>
       </Head>
-      <Row className="justify-content-around">
-        <Col md={6} className="text-center">
-          <h1 className="display-5 fw-bold">
-            Independent Website and Mobile App Development
-          </h1>
-          <p className="lead">
-            Snake Oil Software is proudly owned and operated in&nbsp;
-            <span className="fst-italic">Lubbock, Texas</span>&nbsp; with the
-            aim of providing hands-on development services to individuals and
-            small businesses in the west Texas region. The last few years have
-            shown us all how vital a digital presence is, and we want to help
-            bring you or your business up to date with the current industry
-            standards.
-          </p>
-        </Col>
-        <Col md={5}>
+      <Container className="text-center">
+        <h1 className="display-5 fw-bold">
+          Independent Website and Mobile App Development
+        </h1>
+        <p className="lead">
+          Snake Oil Software is proudly owned and operated in&nbsp;
+          <span className="fst-italic">Lubbock, Texas</span>&nbsp; with the aim
+          of providing hands-on development services to individuals and small
+          businesses in the west Texas region. The last few years have shown us
+          all how vital a digital presence is, and we want to help bring you or
+          your business up to date with the current industry standards.
+        </p>
+      </Container>
+      <Row className="justify-content-center">
+        <Col xs={11} sm={10} md={9} lg={8} xl={6} xxl={5}>
           <Card className="text-dark text-center px-3 py-4">
             <Card.Title>Let&apos;s get in touch!</Card.Title>
             <Card.Text>
