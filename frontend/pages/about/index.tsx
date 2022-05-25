91aba7e8871f84ae64a91be71b30771b849f8b73
Orrin Manning <orrin.manning@protonmail.com>
2022-05-25 08:30:32 -0500

    Fetch all 'about' page data and statically populate the page

--- frontend/pages/about/index.tsx
+++ frontend/pages/about/index.tsx
@@ -1,53 +1,28 @@
 import Head from "next/head";
-import Image from "next/image";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Card from "react-bootstrap/Card";
+import ProfileCard from "../../components/profileCard";
 import { fetchAPI } from "../../lib/api";
 
-const About = ({ about }) => {
+const About = ({ hero, ourTeam }) => {
   return (
     <Container fluid as="main">
       <Head>
         <title>About Snake Oil Software</title>
       </Head>
       <Container as="section">
-        <h1 className="text-center">{about.attributes.hero.heading}</h1>
-        <p>{about.attributes.hero.body}</p>
+        <h1 className="text-center">{hero.heading}</h1>
+        <p>{hero.body}</p>
       </Container>
       <Container as="section">
-        <h1 className="text-center">Our Team</h1>
+        <h1 className="text-center">{ourTeam.heading}</h1>
         <Row>
-          <Col md={6} lg={4}>
-            <Card className="text-dark p-3">
-              <Row className="align-items-center">
-                <Col xs={4}>
-                  <Image
-                    src="/orrin-headshot.jpg"
-                    alt="Headshot of Orrin Manning"
-                    layout="responsive"
-                    width={128}
-                    height={128}
-                    className="rounded-circle"
-                  />
-                </Col>
-                <Col
-                  xs={8}
-                  className="d-flex flex-column justify-content-center ps-0"
-                >
-                  <Card.Title>Orrin Manning</Card.Title>
-                  <Card.Subtitle>Founder, Full Stack Developer</Card.Subtitle>
-                </Col>
-              </Row>
-              <hr />
-              <Card.Text>
-                Studied computer science at the University of Texas at Arlington
-                and professional physics at Texas Tech University, before
-                dropping out to found Snake Oil Software.
-              </Card.Text>
-            </Card>
-          </Col>
+          {ourTeam.profiles.map((profile) => (
+            <Col md={6} lg={4} key={profile.name}>
+              <ProfileCard profile={profile} />
+            </Col>
+          ))}
         </Row>
       </Container>
     </Container>
@@ -58,12 +33,17 @@ export async function getStaticProps() {
   const aboutRes = await fetchAPI("/about", {
     populate: {
       hero: "*",
+      ourTeam: {
+        populate: ["*", "profiles.picture"],
+      },
     },
   });
+  const { hero, ourTeam } = aboutRes.data.attributes;
 
   return {
     props: {
-      about: aboutRes.data,
+      hero: hero,
+      ourTeam: ourTeam,
     },
   };
 }
